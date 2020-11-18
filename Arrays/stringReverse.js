bfdaacc1677dd0f0388efd325dfc041b1095db8f
Chisom Trisha Okoye <trisha.omabu@gmail.com>
2020-11-17 23:30:26 -0700

    Reverse string completed

--- Arrays/stringReverse.js
+++ Arrays/stringReverse.js
@@ -2,7 +2,22 @@
 //'Hi My name is Chisom' should be :
 //'mosihC si eman yM iH'
 
-function reverse(str) {
+//Not so efficient solution
+function reverse1(str) {
+  //input validation
+  if (!str || str.length < 2 || typeof str !== "string") {
+    return "Please enter a valid string";
+  }
+  const reversed = [];
+  const totalItems = str.length - 1;
+  for (let i = totalItems; i >= 0; i--) {
+    reversed.push(str[i]);
+  }
+  return reversed.join("");
+}
+
+//More efficient solution
+function reverse2(str) {
   //input validation
   if (!str || str.length < 2 || typeof str !== "string") {
     return "Please enter a valid string";
@@ -10,4 +25,7 @@ function reverse(str) {
   return str.split("").reverse().join("");
 }
 
-console.log(reverse("Hi My name is Chisom"));
+//ES6 solution without input validation
+const reverse3 = (str) => str.split("").reverse().join("");
+
+console.log(reverse3("Hi My name is Chisom"));
