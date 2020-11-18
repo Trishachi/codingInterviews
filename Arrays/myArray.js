8ad7427abc6d80969e82dba08c07b8cac929f79c
Chisom Trisha Okoye <trisha.omabu@gmail.com>
2020-11-18 00:01:22 -0700

    My Array Class

---
+++ Arrays/myArray.js
@@ -0,0 +1,54 @@
+// create a class to implement an array
+class MyArray {
+  //Array initial function
+  constructor() {
+    this.length = 0;
+    this.data = {};
+  }
+
+  //////////Array Methods ///////////////////////////
+
+  //Access Data
+  get(index) {
+    return this.data[index];
+  }
+
+  //Push Data - Add item to end of array
+  push(item) {
+    this.data[this.length] = item;
+    this.length++;
+    return this.length;
+  }
+
+  //Pop Data - Remove last item in array
+  pop() {
+    const lastItem = this.data[this.length - 1];
+    delete this.data[this.length - 1];
+    this.length--;
+    return lastItem;
+  }
+
+  //Delete Data - Delete data at index
+  delete(index) {
+    const item = this.data[index];
+    this.shiftItems(index);
+  }
+
+  shiftItems(index) {
+    for (let i = index; i < this.length - 1; i++) {
+      this.data[i] = this.data[i + 1];
+    }
+    delete this.data[this.length - 1];
+    this.length--;
+  }
+}
+
+const newArray = new MyArray();
+newArray.push("Hello");
+newArray.push("World");
+newArray.push("!!");
+newArray.push("Howdy");
+newArray.push("Today");
+newArray.push("!!");
+newArray.delete(2);
+console.log(newArray);
