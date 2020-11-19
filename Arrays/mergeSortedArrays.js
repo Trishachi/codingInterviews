9333ccf19d85c9beb9c97b922e345fbaf814569c
Chisom Trisha Okoye <trisha.omabu@gmail.com>
2020-11-18 23:42:34 -0700

    Merge sorted arrays - my method

---
+++ Arrays/mergeSortedArrays.js
@@ -0,0 +1,11 @@
+//Function mergeSortedArrays([0,3,4,31],[4,6,30])
+//Should Return
+// [0, 3, 4, 4, 6, 30, 31]
+
+const mergeSortedArrays = (array1, array2) => {
+  const newArray = [...array1, ...array2];
+  newArray.sort((a, b) => a - b);
+  return newArray;
+};
+
+console.log(mergeSortedArrays([0, 3, 4, 31], [4, 6, 30]));
