45e8f531ed9436ca8e578dc1f6bb301e6c8acef0
Chisom Trisha Okoye <trisha.omabu@gmail.com>
2020-12-14 00:41:06 -0700

    First recurring item WIP

---
+++ Objects/firstRecurringXter.js
@@ -0,0 +1,51 @@
+//Google Question
+//Given an array = [2,5,1,2,3,5,1,2,4]:
+//It should return 2
+
+//Given an array = [2,1,1,2,3,5,1,2,4]:
+//It should return 1
+
+//Given an array = [2,3,4,5]:
+//It should return undefined
+
+//Bonus... What if we had this:
+// [2,5,5,2,3,5,1,2,4]
+// return 5 because the pairs are before 2,2
+
+//Brute force method
+function firstRecurringXter(arr) {
+  for (let i = 0; i < arr.length; i++) {
+    for (let j = i + 1; j < arr.length; j++) {
+      if (arr[i] === arr[j]) {
+        return arr[i];
+      }
+    }
+  }
+  return undefined;
+}
+
+// console.log(firstRecurringXter([2, 5, 5, 2, 3, 5, 1, 2, 4]));
+
+//Brute force method in ES6
+const firstRecurringXter1 = (arr) => {
+  arr.map((item) => console.log(item));
+};
+
+console.log(firstRecurringXter1([2, 3, 4, 5]));
+
+//More Efficient Method
+function firstRecurringXter2(arr) {
+  let map = {};
+  for (let i = 0; i < arr.length; i++) {
+    if (map[arr[i]] !== undefined) {
+      return arr[i];
+    } else {
+      map[arr[i]] = i;
+    }
+    // console.log(map);
+  }
+
+  return undefined;
+}
+
+// console.log(firstRecurringXter2([2, 5, 5, 2, 3, 5, 1, 2, 4]));
