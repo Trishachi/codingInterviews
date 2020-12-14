aa6bf3f2642c01f351488ff9aae527f391ee96c2
Chisom Trisha Okoye <trisha.omabu@gmail.com>
2020-12-13 17:32:42 -0700

    MyObject Implemented

--- Arrays/mergeSortedArrays.js
+++ Arrays/mergeSortedArrays.js
@@ -1,6 +1,7 @@
 //Function mergeSortedArrays([0,3,4,31],[4,6,30])
 //Should Return
 // [0, 3, 4, 4, 6, 30, 31]
+//mergeSortedArrays([0,3,4,31],[]) == [[0,3,4,31]]
 
 const mergeSortedArrays = (array1, array2) => {
   const newArray = [...array1, ...array2];
@@ -8,4 +9,4 @@ const mergeSortedArrays = (array1, array2) => {
   return newArray;
 };
 
-console.log(mergeSortedArrays([0, 3, 4, 31], [4, 6, 30]));
+console.log(mergeSortedArrays([0, 3, 4, 31], []));
