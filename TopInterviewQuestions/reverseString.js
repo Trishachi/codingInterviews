4286e4a92630bae8b8fba0d3023133962fcbda1a
Chisom Trisha Okoye <trisha.omabu@gmail.com>
2021-01-21 23:12:51 -0700

    fizzBuzz added

---
+++ TopInterviewQuestions/reverseString.js
@@ -0,0 +1,15 @@
+//Write a function that reverses a string. The input string is given as an array of characters char[].
+
+// Example 1:
+// Input: ["h","e","l","l","o"]
+// Output: ["o","l","l","e","h"]
+
+// Example 2:
+// Input: ["H","a","n","n","a","h"]
+// Output: ["h","a","n","n","a","H"]
+
+let reverseString = function (s) {
+  return s.reverse();
+};
+
+console.log(reverseString(["H", "a", "n", "n", "a", "h"]));
