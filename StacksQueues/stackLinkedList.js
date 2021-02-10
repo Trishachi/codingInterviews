69acb53b93b780db576800ddd15fe9ccd862d4d0
Chisom Trisha Okoye <trisha.omabu@gmail.com>
2021-02-09 23:29:06 -0700

    Stack Push using Linked List

---
+++ StacksQueues/stackLinkedList.js
@@ -0,0 +1,41 @@
+class Node {
+  constructor(value) {
+    this.value = value;
+    this.next = null;
+  }
+}
+
+class Stack {
+  constructor() {
+    this.top = null;
+    this.bottom = null;
+    this.length = 0;
+  }
+  peek() {
+    return this.top;
+  }
+  push(value) {
+    const newNode = new Node(value);
+    if (this.length === 0) {
+      this.top = newNode;
+      this.bottom = newNode;
+    } else {
+      const holdingPointer = this.top;
+      this.top = newNode;
+      this.top.next = holdingPointer;
+    }
+    this.length++;
+    return this;
+  }
+  pop() {}
+  //isEmpty
+}
+
+const myStack = new Stack();
+console.log(myStack.push("google"));
+console.log(myStack.push("udemy"));
+// console.log(myStack.peek());
+
+//Discord
+//Udemy
+//google
