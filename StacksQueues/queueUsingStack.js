ac2349b8f0c7730a9a21327d1026106b5f84ccfc
Chisom Trisha Okoye <trisha.omabu@gmail.com>
2021-02-12 23:44:17 -0700

    Queues

---
+++ StacksQueues/queueUsingStack.js
@@ -0,0 +1,15 @@
+class stackQueue {
+  constructor() {
+    this.first = [];
+    this.last = [];
+  }
+
+  enqueue(value) {}
+
+  dequeue() {}
+
+  peek() {}
+}
+
+const myQueue = new stackQueue();
+comsole.log(myQueue.peek());
