254e8581b3501a579398408bd59af6e2370b78f8
Chisom Trisha Okoye <trisha.omabu@gmail.com>
2021-06-05 01:26:41 -0600

    Making AJAX Calls

---
+++ WebDev/useAjax.js
@@ -0,0 +1,17 @@
+//AJAX - Asynchronous JS and XML, uses XMLHttpRequest, send and open mmethods.
+
+var data;
+var request = new XMLHttpRequest();
+
+request.open("GET", "js/data.json"); //prepares the request
+
+//for tracking the returned response
+request.onreadystatechange = function () {
+  if (request.status === 200 && request.readyState === 4) {
+    data = JSON.parse(request.responseText);
+    console.log(data);
+    console.log(request);
+  }
+};
+
+request.send(""); //sends the request
