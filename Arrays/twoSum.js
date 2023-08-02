e745a8e5cfc960913535fcc884524719122953ca
Chisom Trisha Okoye <trisha.omabu@gmail.com>
2023-08-02 00:25:55 -0600

    Added Solution considerations

--- Arrays/twoSum.js
+++ Arrays/twoSum.js
@@ -1,4 +1,4 @@
-// Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
+// Given an array of integer nums and an integer target, return indices of the two numbers such that they add up to target.
 
 // You may assume that each input would have exactly one solution, and you may not use the same element twice.
 
@@ -17,6 +17,22 @@
 
 // Input: nums = [3,3], target = 6
 // Output: [0,1]
+
+///////////////////////////////////////
+//CLARIFICATION QUESTIONS
+//- Is my input always going to be an array
+//- Is my input array sorted
+//- What do I output if the target sum is not found within the given array
+
+//Input validations
+  //Check if any of the input arrays is empty
+
+//Steps
+  //Loop through the array and form a harsh table of item and index
+  //Loop through the harsh table to find target sum
+  //Return indexs of current item and target sum if found
+  //Return target not found if not found
+
 
 ///////////////////////////////////////////////////////////////////////////
 //SOLUTION
