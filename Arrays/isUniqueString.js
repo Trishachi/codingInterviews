04c596b05b190849cfe9982629e8ff18f351d842
Chisom Trisha Okoye <trisha.omabu@gmail.com>
2023-08-03 17:19:39 -0600

    Update isUniqueString.js
    Not the most efficient

--- Arrays/isUniqueString.js
+++ Arrays/isUniqueString.js
@@ -1,1 +1,39 @@
-//Implement an algorithim to determine if a string has all unique characters
+//Implement an algorithm to determine if a string has all unique characters
+
+//Tests
+// 
+
+//Questions
+// - is the input string an ASCII code or Unicode string?
+// - What is my expected output format?
+// - Is the input string case-sensitive?
+
+//Assumptions
+//- Assume ASCII String or extended ASCII string - ie max 128 chars or 256 chars for extended ASCII
+
+//Brute Force
+
+//Steps
+ //- Split string characters to array
+//- convert items of array to object and count number of occurrence
+//- If any item occurs more than once -return flase
+// - Else return true
+
+
+const isUniqueChars = (str) => {
+  const itemCount = {};  
+  if (str.length > 256) {return false};
+  const strToArr = str.toLowerCase().split("");
+  for (let item in strToArr){
+    if(itemCount[strToArr[item]]){
+      itemCount[strToArr[item]] += 1;
+      if(itemCount[strToArr[item]] > 1)
+        { return false;}
+    } else {
+      itemCount[strToArr[item]] = 1;
+    }
+  }
+  return true;
+ }
+
+console.log(isUniqueChars("Chisomili"));
