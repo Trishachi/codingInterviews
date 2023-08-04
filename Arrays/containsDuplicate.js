5eab99dd2190f75f4205b2cd5bcbab401ab551fd
Chisom Trisha Okoye <trisha.omabu@gmail.com>
2023-08-03 22:38:50 -0600

    Create containsDuplicate.js

---
+++ Arrays/containsDuplicate.js
@@ -0,0 +1,17 @@
+//Given an integer array nums, 
+//return true if any value appears at least twice in the array, 
+//and return false if every element is distinct.
+
+// Tests
+//containsDuplicate([1,2,3,1]) = True
+//containsDuplicate([1,2,3,4]) = False
+//containsDuplicate([1,1,1,3,3,4,3,2,4,2]) = True
+
+//Steps
+//- convert items of array to object and count number of occurence
+//- If any item occurs more than once -return flase
+// - Else retun true
+
+var containsDuplicate = function(nums) {
+    
+};
